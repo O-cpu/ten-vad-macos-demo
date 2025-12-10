56f74852d14e63cefbcaf112e98bd235d067a478
Oliver Stuckert <oliver.stuckert@mobilezone.org>
2025-12-10 17:42:14 +0100

    Initial Commit

---
+++ TEN_VAD_TEST/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TEN_VAD_TEST
+//
+//  Created by Oiver Stuckert on 10.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TEN_VAD_TEST/TEN_VAD_TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEN_VAD_TESTApp.swift
+//  TEN_VAD_TEST
+//
+//  Created by Oiver Stuckert on 10.12.25.
+//
+
+import SwiftUI
+
+@main
+struct TEN_VAD_TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
